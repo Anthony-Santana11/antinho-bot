6df1daf2f8b191e9146b9fe3ec2fd701c46483fb
Anthony-Santana11 <anthonysantana110306@gmail.com>
2026-04-07 19:25:11 -0300

    implement Discord bot functionality and update dependencies in pom.xml

--- src/main/java/br/com/anthony/bot/antinho/AntinhoApplication.java
+++ src/main/java/br/com/anthony/bot/antinho/AntinhoApplication.java
@@ -1,13 +1,32 @@
 package br.com.anthony.bot.antinho;
 
+import br.com.anthony.bot.antinho.service.AntinhoBotListener;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @SpringBootApplication
 public class AntinhoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AntinhoApplication.class, args);
-	}
 
-}
+    private static String token;
+
+    @Value("${DISCORD_BOT_TOKEN}")
+    public void setToken(String token) {
+        AntinhoApplication.token = token;
+    }
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(AntinhoApplication.class, args);
+        JDA builder = JDABuilder.createDefault(token)
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                .setActivity(Activity.playing("fale comigo mencionando @antinho"))
+                .addEventListeners(new AntinhoBotListener(new br.com.anthony.bot.antinho.service.ChatService().doRag()))
+                .build()
+                .awaitReady();
+    }
+    }
--- src/test/java/br/com/anthony/bot/antinho/AntinhoApplicationTests.java
+++ src/test/java/br/com/anthony/bot/antinho/AntinhoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AntinhoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
