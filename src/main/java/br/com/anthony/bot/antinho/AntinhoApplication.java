a544c5fd153f08d81ff46e030018005e0a198b15
Anthony-Santana11 <anthonysantana110306@gmail.com>
2026-04-07 19:22:26 -0300

    initialize project structure with Maven wrapper and basic bot functionality

---
+++ src/main/java/br/com/anthony/bot/antinho/AntinhoApplication.java
@@ -0,0 +1,13 @@
+package br.com.anthony.bot.antinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntinhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/anthony/bot/antinho/AntinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.anthony.bot.antinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
